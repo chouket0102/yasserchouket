daf7ac6a2b069cccd37330bff3752828d20a3bce
yasserchouket <yasserchouket0102@gmail.com>
2025-06-13 11:12:10 +0100

    add astro vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   vite: {
     resolve: {
       alias: {
@@ -15,7 +19,6 @@ export default defineConfig({
       },
     },
   },
-  output: "static",
   build: {
     inlineStylesheets: "auto",
   },
